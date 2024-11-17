395a345ef1b262e9046d956ddd0843d038d37f16
k3rb3ros <development@k3rb3ros.ninja>
2024-11-16 17:00:47 -0800

    refactor and add additional b tree functions

---
+++ b_tree/b_tree.h
@@ -0,0 +1,103 @@
+#ifndef B_TREE_H
+#define B_TREE_H
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+/**************
+* definitions *
+**************/
+
+// TODO refine these constants
+#define MAX_KEYS 4
+#define DATA_NULL -1
+#define DEPTH_NULL -1
+
+#define MAX_NODES 1024
+// TODO replace with tree meta data
+#define MAX_DEPTH 4
+#define MAX_SUBNODES (MAX_KEYS + 1)
+
+
+/********************
+* types and structs *
+********************/
+
+typedef int32_t node_data_t;
+typedef struct b_node_s b_node_t;
+
+/* node structure is such that
+* data[0] < next[0]->keys || next[0]->keys == NULL
+* next[0]->keys < data[1] < next[1]->keys || next[1]->keys == NULL
+*/
+
+struct b_node_s {
+	node_data_t keys[MAX_KEYS];
+	ssize_t num_keys;
+	b_node_t* next[MAX_SUBNODES];
+	ssize_t depth;
+};
+
+
+/*************
+* comparator *
+*************/
+
+static inline bool compare_data(const node_data_t a, const node_data_t b) {
+	return a < b;
+}
+
+
+/******************
+* tree operations *
+******************/
+
+void b_tree_insert(b_node_t* node, const node_data_t key);
+
+void b_tree_insert_not_full(b_node_t* node, const node_data_t key);
+
+void b_tree_split_child(b_node_t* node);
+
+b_node_t* b_tree_search(b_node_t* node, const node_data_t key);
+
+void b_tree_traverse(const b_node_t* node);
+
+void b_tree_delete(b_node_t* node, const node_data_t key);
+
+
+/**********
+* utility *
+**********/
+
+static inline bool is_b_tree_node_leaf(const b_node_t* node) {
+	for (size_t s=0; s<MAX_SUBNODES; ++s) {
+		if (node->next[s] != NULL) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
+static inline bool is_b_tree_node_empty(const b_node_t* node) {
+    bool is_empty = true;
+
+    // only leaf leaf nodes can be empty
+    if (is_b_tree_node_leaf(node)) {
+        for (size_t s=0; s<MAX_KEYS; ++s) {
+            // any non NULL keys in a node indicate its not empty
+            if (node->keys[s] != DATA_NULL) {
+                is_empty = false;
+                break;
+            }
+        }
+    }
+
+    is_empty = node->depth > DEPTH_NULL ? false : is_empty;
+    return is_empty;
+}
+
+void print_tree(b_node_t* n);
+
+#endif
